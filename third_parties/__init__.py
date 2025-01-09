2059d57a5305d6b7a0eea252894ad1065e72f40d
Alkiviadis Tzaras <alkiviadis.tzaras@efta.int>
2025-01-09 11:17:10 +0100

    test

---
+++ third_parties/__init__.py
@@ -0,0 +1,12 @@
+import os 
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock:bool = False):
+    """
+    Scrape information from Linkedin profiles,
+    Manually scrape the information from the Lin
+    """
